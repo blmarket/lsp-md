e01fde717f5809f7979d5e347a74f7973c15b25d
Heon Jeong <blmarket@gmail.com>
2023-11-05 11:50:52 -0500

    Test read markdown file

---
+++ examples/read_md.rs
@@ -0,0 +1,18 @@
+
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+  let mut file = File::open("/home/blmarket/note.md").await?;
+  let mut str: String = String::new();
+  file.read_to_string(&mut str).await?;
+  dbg!(&str.len());
+  let mut splits = str.split("\n# ");
+  dbg!(splits.next());
+  for it in splits {
+    dbg!(&it[0..10]);
+  }
+
+  Ok(())
+}
