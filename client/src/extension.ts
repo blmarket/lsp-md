704151453a34f648875b27f2f9f89b024143b03c
Heon Jeong <blmarket@gmail.com>
2023-11-12 20:15:25 -0500

    Remove unused client functionalities
    They won't be used

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,27 +4,12 @@
  * ------------------------------------------------------------------------------------------ */
 
 import {
-  languages,
   workspace,
-  EventEmitter,
   ExtensionContext,
   window,
-  InlayHintsProvider,
-  TextDocument,
-  CancellationToken,
-  Range,
-  InlayHint,
-  TextDocumentChangeEvent,
-  ProviderResult,
-  commands,
-  WorkspaceEdit,
-  TextEdit,
-  Selection,
-  Uri,
 } from "vscode";
 
 import {
-  Disposable,
   Executable,
   LanguageClient,
   LanguageClientOptions,
@@ -32,25 +17,8 @@ import {
 } from "vscode-languageclient/node";
 
 let client: LanguageClient;
-// type a = Parameters<>;
 
 export async function activate(context: ExtensionContext) {
-  let disposable = commands.registerCommand("helloworld.helloWorld", async uri => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    const url = Uri.parse('/home/victor/Documents/test-dir/nrs/another.nrs')
-    let document = await workspace.openTextDocument(uri);
-    await window.showTextDocument(document);
-    
-    // console.log(uri)
-    window.activeTextEditor.document
-    let editor = window.activeTextEditor;
-    let range = new Range(1, 1, 1, 1)
-    editor.selection = new Selection(range.start, range.end);
-  });
-
-  context.subscriptions.push(disposable);
-
   const traceOutputChannel = window.createOutputChannel("Nrs Language Server trace");
   const command = process.env.SERVER_PATH || "nrs-language-server";
   const run: Executable = {
@@ -82,7 +50,6 @@ export async function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient("nrs-language-server", "nrs language server", serverOptions, clientOptions);
-  // activateInlayHints(context);
   client.start();
 }
 
@@ -91,82 +58,4 @@ export function deactivate(): Thenable<void> | undefined {
     return undefined;
   }
   return client.stop();
-}
-
-export function activateInlayHints(ctx: ExtensionContext) {
-  const maybeUpdater = {
-    hintsProvider: null as Disposable | null,
-    updateHintsEventEmitter: new EventEmitter<void>(),
-
-    async onConfigChange() {
-      this.dispose();
-
-      const event = this.updateHintsEventEmitter.event;
-      // this.hintsProvider = languages.registerInlayHintsProvider(
-      //   { scheme: "file", language: "nrs" },
-      //   // new (class implements InlayHintsProvider {
-      //   //   onDidChangeInlayHints = event;
-      //   //   resolveInlayHint(hint: InlayHint, token: CancellationToken): ProviderResult<InlayHint> {
-      //   //     const ret = {
-      //   //       label: hint.label,
-      //   //       ...hint,
-      //   //     };
-      //   //     return ret;
-      //   //   }
-      //   //   async provideInlayHints(
-      //   //     document: TextDocument,
-      //   //     range: Range,
-      //   //     token: CancellationToken
-      //   //   ): Promise<InlayHint[]> {
-      //   //     const hints = (await client
-      //   //       .sendRequest("custom/inlay_hint", { path: document.uri.toString() })
-      //   //       .catch(err => null)) as [number, number, string][];
-      //   //     if (hints == null) {
-      //   //       return [];
-      //   //     } else {
-      //   //       return hints.map(item => {
-      //   //         const [start, end, label] = item;
-      //   //         let startPosition = document.positionAt(start);
-      //   //         let endPosition = document.positionAt(end);
-      //   //         return {
-      //   //           position: endPosition,
-      //   //           paddingLeft: true,
-      //   //           label: [
-      //   //             {
-      //   //               value: `${label}`,
-      //   //               // location: {
-      //   //               //   uri: document.uri,
-      //   //               //   range: new Range(1, 0, 1, 0)
-      //   //               // }
-      //   //               command: {
-      //   //                 title: "hello world",
-      //   //                 command: "helloworld.helloWorld",
-      //   //                 arguments: [document.uri],
-      //   //               },
-      //   //             },
-      //   //           ],
-      //   //         };
-      //   //       });
-      //   //     }
-      //   //   }
-      //   // })()
-      // );
-    },
-
-    onDidChangeTextDocument({ contentChanges, document }: TextDocumentChangeEvent) {
-      // debugger
-      // this.updateHintsEventEmitter.fire();
-    },
-
-    dispose() {
-      this.hintsProvider?.dispose();
-      this.hintsProvider = null;
-      this.updateHintsEventEmitter.dispose();
-    },
-  };
-
-  workspace.onDidChangeConfiguration(maybeUpdater.onConfigChange, maybeUpdater, ctx.subscriptions);
-  workspace.onDidChangeTextDocument(maybeUpdater.onDidChangeTextDocument, maybeUpdater, ctx.subscriptions);
-
-  maybeUpdater.onConfigChange().catch(console.error);
 }
