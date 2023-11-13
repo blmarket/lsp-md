eafcb73d093a6b68626acaa70908c29a96f43635
Heon Jeong <blmarket@gmail.com>
2023-11-13 17:29:04 -0500

    Change BertModels to have static lifetime
    In order to use the model with LanguageServer it should have static
    lifetime.

--- src/document/bert/model.rs
+++ src/document/bert/model.rs
@@ -11,11 +11,11 @@ use super::{
     Encoder,
 };
 
-pub struct BertModels<'a> {
-    model: KeywordExtractionModel<'a>,
+pub struct BertModels {
+    model: KeywordExtractionModel<'static>,
 }
 
-impl<'a> Default for BertModels<'a> {
+impl Default for BertModels {
     fn default() -> Self {
         let model = KeywordExtractionModel::new(KeywordExtractionConfig {
             sentence_embeddings_config: SentenceEmbeddingsConfig::from(
@@ -28,7 +28,7 @@ impl<'a> Default for BertModels<'a> {
     }
 }
 
-impl<'a> Encoder for BertModels<'a> {
+impl Encoder for BertModels {
     fn encode_batch(
         &self,
         sentences: &[&str],
@@ -46,7 +46,7 @@ impl<'a> Encoder for BertModels<'a> {
     }
 }
 
-impl<'a> Keywords for BertModels<'a> {
+impl Keywords for BertModels {
     fn extract_batch(
         &self,
         texts: &[&str],
@@ -58,5 +58,42 @@ impl<'a> Keywords for BertModels<'a> {
             .into_iter()
             .map(|v2| v2.into_iter().map(|v3| v3.into()).collect())
             .collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_local_model_encode() -> anyhow::Result<()> {
+        let model = BertModels::default();
+        let sentences =
+            vec!["This is a test sentence.", "This is another test sentence."];
+        let embeddings = model.encode_batch(&sentences)?;
+        assert_eq!(embeddings.len(), 2);
+
+        let embedding = model.encode(&sentences[0])?;
+        assert!(embeddings[0].dist(&embedding) < 0.001f32);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_local_model_encode_single() -> anyhow::Result<()> {
+        let model = BertModels::default();
+        let sentence = "This is a test sentence.";
+        let _ = model.encode(sentence)?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_load_model_within_tokio() -> anyhow::Result<()> {
+        let model =
+            tokio::task::spawn_blocking(|| BertModels::default()).await?;
+        let embedding = model.encode("This is a test sentence.")?;
+        let zero = Embedding::new(vec![1f32; 384]);
+        assert!(zero.dist(&embedding) > 0.1);
+        Ok(())
     }
 }
