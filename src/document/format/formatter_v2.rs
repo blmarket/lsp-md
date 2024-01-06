d62db52d541e843ef42e6be013e16cd38d93644e
Jeong, Hun <blmarket@gmail.com>
2024-01-05 21:41:33 -0800

    Small implementation of incremental updates

--- src/document/format/formatter_v2.rs
+++ src/document/format/formatter_v2.rs
@@ -1,15 +1,18 @@
 #![allow(dead_code)]
 
 use ropey::Rope;
-use tree_sitter::{Parser, Tree};
+use tower_lsp::lsp_types::{TextDocumentContentChangeEvent, Position as LspPosition};
+use tree_sitter::{Parser, Tree, InputEdit, Point};
 
-pub struct Formatter<'a> {
-    buf: &'a Rope,
+use crate::document::{incremental_sync::IncrementalSync, document_adapter::LspAdapter as _};
+
+pub struct Formatter {
+    buf: Rope,
     tree: Tree,
 }
 
-impl<'a> Formatter<'a> {
-    pub fn new(buf: &'a Rope) -> Self {
+impl Formatter {
+    pub fn new(buf: Rope) -> Self {
         let lang = tree_sitter_md::language();
         let mut parser = Parser::new();
         parser.set_language(lang).expect("should set lang");
@@ -27,16 +30,145 @@ impl<'a> Formatter<'a> {
         
         Self { buf, tree }
     }
+    
+    fn apply_edit(updated: Rope, mut old_tree: Tree, edit: InputEdit) -> Self {
+        let lang = tree_sitter_md::language();
+        let mut parser = Parser::new();
+        parser.set_language(lang).expect("should set lang");
+
+        old_tree.edit(&edit);
+        let tree = parser
+            .parse_with(
+                &mut |offset, _| {
+                    let (chunk_str, chunk_byte_idx, _, _) =
+                        updated.chunk_at_byte(offset);
+                    &chunk_str.as_bytes()[offset - chunk_byte_idx..]
+                },
+                Some(&old_tree),
+            )
+            .expect("should parse doc");
+
+        Self { buf: updated, tree }
+    }
+}
+
+trait LspPositionToPoint {
+    fn to_point(&self) -> Point;
+}
+
+impl LspPositionToPoint for LspPosition {
+    fn to_point(&self) -> Point {
+        Point {
+            row: self.line as usize,
+            column: self.character as usize,
+        }
+    }
+}
+
+impl IncrementalSync for Formatter {
+    fn apply_change(mut self, change: TextDocumentContentChangeEvent) -> Self {
+        let Some(rng) = change.range else {
+            return Formatter::new(self.buf.apply_change(change));
+        };
+        
+        let sp = self.buf.position_to_offset(&rng.start).unwrap();
+        let old_ep = self.buf.position_to_offset(&rng.end).unwrap();
+        let new_ep = sp + change.text.as_bytes().len();
+        
+        let updated_rope = self.buf.apply_change(change);
+        
+        let new_end_position = updated_rope.offset_to_position(new_ep).unwrap();
+        
+        let input_edit = InputEdit {
+            start_byte: sp,
+            old_end_byte: old_ep,
+            new_end_byte: new_ep,
+            start_position: rng.start.to_point(),
+            old_end_position: rng.end.to_point(),
+            new_end_position: new_end_position.to_point(),
+        };
+        
+        Formatter::apply_edit(updated_rope, self.tree, input_edit)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::document::format::treesitter::debug_walk;
+
     use super::*;
     use ropey::Rope;
 
     #[test]
-    fn test_format() {
+    fn new_works() {
         let buf = Rope::from("Hello world\n한글\n".repeat(10000));
-        let formatter = Formatter::new(&buf);
+        let formatter = Formatter::new(buf);
+        debug_walk(formatter.tree.walk());
+    }
+
+    #[test]
+    fn partial_update_should_work() {
+        let buf = Rope::from("## Title\n\nSome paragraph\n\n```\nlet a = 1;\n```\n\n");
+        let formatter = Formatter::new(buf);
+        debug_walk(formatter.tree.walk());
+        
+        let updated = formatter.apply_change(TextDocumentContentChangeEvent {
+            range: Some(tower_lsp::lsp_types::Range {
+                start: tower_lsp::lsp_types::Position {
+                    line: 4,
+                    character: 0,
+                },
+                end: tower_lsp::lsp_types::Position {
+                    line: 5,
+                    character: 0,
+                },
+            }),
+            range_length: None,
+            text: "".to_string(),
+        });
+        
+        debug_walk(updated.tree.walk());
+    }
+
+    #[test]
+    fn partial_update_should_handle_big_tree_changes() {
+        let src = "## Title\n\n".to_string() + &"Some paragraph\n\n".repeat(10000);
+        let buf = Rope::from_str(&src);
+        let formatter = Formatter::new(buf);
+        debug_walk(formatter.tree.walk());
+        
+        let updated = formatter.apply_change(TextDocumentContentChangeEvent {
+            range: Some(tower_lsp::lsp_types::Range {
+                start: tower_lsp::lsp_types::Position {
+                    line: 4,
+                    character: 0,
+                },
+                end: tower_lsp::lsp_types::Position {
+                    line: 5,
+                    character: 0,
+                },
+            }),
+            range_length: None,
+            text: "```\n".to_string(),
+        });
+        
+        debug_walk(updated.tree.walk());
+
+        let updated2 = updated.apply_change(TextDocumentContentChangeEvent {
+            range: Some(tower_lsp::lsp_types::Range {
+                start: tower_lsp::lsp_types::Position {
+                    line: 20002,
+                    character: 0,
+                },
+                end: tower_lsp::lsp_types::Position {
+                    line: 20002,
+                    character: 0,
+                },
+            }),
+            range_length: None,
+            text: "```\n".to_string(),
+        });
+        
+        debug_walk(updated2.tree.walk());
     }
 }
